46fb1dbcb6ac0db909a6d8ac40fad7f66607c031
sandeep9618 <122001039+sandeep9618@users.noreply.github.com>
2023-02-03 14:08:15 +0530

    Update index.js

--- src/components/ContactItem/index.js
+++ src/components/ContactItem/index.js
@@ -1,15 +1,15 @@
 import './index.css'
 
 const ContactItem = props => {
-  const {contactDetails, toggleStyles} = props
+  const {contactDetails, toggleIsFavorite} = props
   const {name, mobileNo, isFavorite, id} = contactDetails
 
   const starImgUrl = isFavorite
     ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
 
-  const toggleFavButton = () => {
-    toggleStyles(id)
+  const onClickFavoriteIcon = () => {
+    toggleIsFavorite(id)
   }
 
   return (
@@ -23,7 +23,7 @@ const ContactItem = props => {
         <button
           type="button"
           className="favorite-icon-container"
-          onClick={toggleFavButton}
+          onClick={onClickFavoriteIcon}
         >
           <img src={starImgUrl} className="favorite-icon" alt="star" />
         </button>
